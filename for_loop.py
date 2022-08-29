f95041526762f706747cdffcdc2db8c95c9fecdf
Jeremiahkwiha <jeremiahnsaba34@gmail.com>
2022-08-29 16:51:36 +0300

    loops

---
+++ for_loop.py
@@ -0,0 +1,37 @@
+fruits= ["banana","apple","mango","orange"]
+for fruit in fruits:
+    print(fruit)
+
+# 2, 4, 6, 8
+
+for i in range(5):
+    print(i)
+
+#print(list(range(5)))
+#for nu, in range(1, 21, 2):
+    
+
+
+#len() return the size of a list
+
+fruits = ["banana","apple","mango","orange"]
+for i in range(len(fruits)):
+    print("current fruit:", fruits[i])
+
+
+digits = [0,1,5]
+for digit in digits:
+    print(digit)
+else:
+    print("no item left")
+
+
+
+
+students_name = "Arthur"
+
+marks = {"James":90,"school":"KYU","age":24}
+print(marks)
+
+
+
